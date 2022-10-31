018cbcbd6f7abc3030601e0c391b033c3111d735
DaeHyeon Kim <98071015+fito-daehyeon@users.noreply.github.com>
2022-11-01 03:47:42 +0900

    뉴스 앱 프로젝트 시작

---
+++ NewsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NewsApp
+//
+//  Created by DaeHyeonKim on 2022/10/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by DaeHyeonKim on 2022/10/31.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
